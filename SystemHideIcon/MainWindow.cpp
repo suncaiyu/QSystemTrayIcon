19db2b20031ffe75bfbde0232d27995f401ccba8
suncaiyu <42695008+suncaiyu@users.noreply.github.com>
2019-10-29 00:26:14 +0800

    1.0

---
+++ SystemHideIcon/MainWindow.cpp
@@ -0,0 +1,79 @@
+﻿#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(&clickTimer, SIGNAL(timeout()), this, SLOT(slotTimerTimeOut()));
+    hideIcon = new QSystemTrayIcon();
+    hideIcon->setIcon(QIcon(":/Pic/2.jpg"));
+    myMenu = new QMenu();
+    quit = new QAction();
+    showWindow = new QAction();
+    showWindow->setIcon(QIcon(":/Pic/22.jpg"));
+    quit->setIcon(QIcon(":/Pic/222.jpg"));
+    showWindow->setText(QString::fromLocal8Bit("显示"));
+    quit->setText(QStringLiteral("退出"));
+    myMenu->addAction(showWindow);
+    myMenu->addAction(quit);
+    hideIcon->setContextMenu(myMenu);
+    connect(showWindow, SIGNAL(triggered()), this, SLOT(slotShow()));
+    connect(quit, SIGNAL(triggered()), this, SLOT(close()));
+    connect(hideIcon,
+            SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this,
+            SLOT(slotActivated(QSystemTrayIcon::ActivationReason)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    clickTimer.start(250);
+}
+
+void MainWindow::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    clickTimer.stop();
+    qDebug() << "double click";
+}
+
+void MainWindow::slotTimerTimeOut()
+{
+    clickTimer.stop();
+    qDebug() << "click";
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e)
+{
+    if (e->modifiers() == Qt::ControlModifier && e->key() == Qt::Key_Q) {
+        this->hide();
+        hideIcon->show();
+        hideIcon->showMessage(QStringLiteral("提示信息"),
+                              QStringLiteral("已隐藏到系统托盘!"),
+                              QSystemTrayIcon::Information,
+                              3000);
+    }
+}
+
+void MainWindow::slotShow()
+{
+    this->show();
+    hideIcon->hide();
+}
+
+void MainWindow::slotActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (reason) {
+    case QSystemTrayIcon::Trigger:
+        this->show();
+        hideIcon->hide();
+        break;
+    default:
+        break;
+    }
+}
---
+++ SystemHideIcon/MainWindow.h
@@ -0,0 +1,42 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSystemTrayIcon>
+#include <QTimer>
+#include <QMenu>
+#include <QAction>
+#include <QKeyEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void mousePressEvent(QMouseEvent *e);
+    void mouseDoubleClickEvent(QMouseEvent *e);
+    void keyPressEvent(QKeyEvent *e);
+private slots:
+    void slotTimerTimeOut();
+    void slotShow();
+    void slotActivated(QSystemTrayIcon::ActivationReason reason);
+
+private:
+    Ui::MainWindow *ui;
+    QTimer clickTimer;
+    QSystemTrayIcon *hideIcon;
+    QMenu *myMenu;
+    QAction *showWindow;
+    QAction *quit;
+};
+#endif // MAINWINDOW_H
